5373e28f9a9f669a21c4feb35367133b8070f89c
IvanArsy <ivan22003@mail.unpad.ac.id>
2025-06-06 17:35:42 +0700

    vite

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,15 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/', // Gunakan '/' untuk root domain (subdomain Railway)
-  server: {
-    host: true, // Penting untuk Railway
-    port: process.env.PORT || 3000,
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
-  preview: {
-    port: process.env.PORT || 3000,
-  }
 });
